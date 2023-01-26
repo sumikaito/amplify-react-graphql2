0bcb98867133ea28f34d1a85d7373cfeb390a734
sumikaito <sumika.ito@ctc-g.co.jp>
2023-01-26 19:56:49 +0900

    added api db

--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -1,4 +1,11 @@
 export type AmplifyDependentResourcesAttributes = {
+  "api": {
+    "notesapp": {
+      "GraphQLAPIEndpointOutput": "string",
+      "GraphQLAPIIdOutput": "string",
+      "GraphQLAPIKeyOutput": "string"
+    }
+  },
   "auth": {
     "amplifyreactgraphql207716d47": {
       "AppClientID": "string",
